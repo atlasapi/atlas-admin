091e25f46d0b11132de9dda1d18c3d9d6284f112
liamgh <liam@greenhughes.com>
2013-09-30 16:14:59 +0100

    Drag and drop directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,84 @@
 /* Directives */
 
 
-angular.module('atlasAdmin.directives', []).
-  directive('appVersion', ['version', function(version) {
+var app = angular.module('atlasAdmin.directives', []);
+
+app.directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
+
+
+app.directive('orderable', function() {
+   return function(scope, element) {
+        // this gives us the native JS object
+        var el = element[0];
+
+        el.draggable = true;
+
+        el.addEventListener(
+            'dragstart',
+            function(e) {
+                e.dataTransfer.effectAllowed = 'move';
+                e.dataTransfer.setData('Text', this.id);
+                e.dataTransfer.setData('y', e.pageY);
+                this.classList.add('drag');
+                return false;
+            },
+            false
+        );
+
+        el.addEventListener(
+            'dragend',
+            function(e) {
+                this.classList.remove('drag');
+                return false;
+            },
+            false
+        );
+       el.addEventListener(
+            'dragover',
+            function(e) {
+              e.dataTransfer.dropEffect = 'move';
+              // allows us to drop
+              e.preventDefault();
+              this.classList.add('over');
+              return false;
+            },
+            false
+      );
+      el.addEventListener(
+            'dragleave',
+            function(e) {
+              e.dataTransfer.dropEffect = 'move';
+              // allows us to drop
+              if (e.preventDefault) e.preventDefault();
+                this.classList.remove('over');
+                return false;
+              },
+              false
+          );
+     el.addEventListener(
+         'drop',
+         function(e) {
+           // Stops some browsers from redirecting.
+           if (e.preventDefault) e.preventDefault(); // Stops FF from redirecting
+           if (e.stopPropagation) e.stopPropagation();
+              this.classList.remove('over');
+              var item = document.getElementById(e.dataTransfer.getData('Text'));
+              var isDown = e.dataTransfer.getData('y') < e.pageY;
+              if (isDown) {
+                // insert after
+                this.parentNode.insertBefore(item, this.nextSibling);
+              } else {
+                // insert before
+                this.parentNode.insertBefore(item, item.previousSibling);
+              }
+              return false;
+         },
+         false
+     );
+       
+   }    
+});
