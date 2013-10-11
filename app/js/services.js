a29cf6b55b23526fcaee383f583c5c5516a7c745
liamgh <liam@greenhughes.com>
2013-10-11 11:21:11 +0100

    Create service for Atlas rather than calling http directly

--- app/js/services.js
+++ app/js/services.js
@@ -2,76 +2,87 @@
 
 /* Services */
 angular.module('atlasAdmin.services', [])
- .factory('Sources', function ($http, atlasHost, Applications) {
+ .factory('Sources', function (Atlas, Applications) {
     return {
         all: function () {
-             return $http.get(atlasHost + '/sources.json').then(function(result) {return result.data.sources;});
+             return Atlas.getRequest('/sources.json').then(function(result) {return result.data.sources;});
         },
         get: function (sourceId) {
-             return $http.get(atlasHost + '/sources/' + sourceId + '.json').then(function(result) {return result.data.source;});
+             return Atlas.getRequest('/sources/' + sourceId + '.json').then(function(result) {return result.data.source;});
         },
         changeAppState: function(sourceId, appId, state, callback) {
             var data = {};
-            var url = atlasHost + "/sources/" + sourceId + "/applications/readers/" + appId + "/state?state="+ state;
-            $http.post(url, data, {withCredentials: false}).success(callback);//.error(function(error) {console.log(error)});
+            var url = "/sources/" + sourceId + "/applications/readers/" + appId + "/state?state="+ state;
+            Atlas.postRequest(url, data, {withCredentials: false}).success(callback);//.error(function(error) {console.log(error)});
         },
         addWriter: function(sourceId, appId, callback) {
-            var url = atlasHost + "/sources/" + sourceId + "/applications?id=" + appId + "&permission=write";
-            $http.post(url, {}, {withCredentials: false}).success(callback);
+            var url = "/sources/" + sourceId + "/applications?id=" + appId + "&permission=write";
+            Atlas.postRequest(url, {}, {withCredentials: false}).success(callback);
         }
     }
  })
-.factory('Applications', function ($http, atlasHost) {
+.factory('Applications', function (Atlas) {
     return {
         all: function () {
-            return $http.get(atlasHost + '/applications.json').then(function (results) {return results.data.applications});
+            return Atlas.getRequest('/applications.json').then(function (results) {return results.data.applications});
         },
         get: function(applicationId) {
-            return $http.get(atlasHost + '/applications/' + applicationId + '.json').then(function (results) {return results.data.application});   
+            return Atlas.getRequest('/applications/' + applicationId + '.json').then(function (results) {return results.data.application});   
         },
         create: function(title) {
-            var url = atlasHost + "/applications.json";
             var application = {
                  "title": title,
                  "description":"",
                  "publisher": {"key":"metabroadcast.com", "name":"MetaBroadcast","country":"ALL"}
             }
-            return $http.post(url, application, {withCredentials: false});
+            return Atlas.postRequest("/applications.json", application);
         },
         update: function(application, callback) {
-            var url = atlasHost + "/applications.json";
-            return $http.post(url, application, {withCredentials: false});
+            return Atlas.postRequest("/applications.json", application);
         },
         setPrecedence: function(applicationId, sourceIdOrder) {
-            var url = atlasHost + "/applications/" + applicationId + "/precedence";
+            var url = "/applications/" + applicationId + "/precedence";
             var data = {"ordering":sourceIdOrder};
-            return $http.post(url, data, {withCredentials: false});
+            return Atlas.postRequest(url, data);
         },
         deletePrecedence:  function(applicationId) {
-            var url = atlasHost + "/applications/" + applicationId + "/precedence";
-            return $http.delete(url);
+            var url = "/applications/" + applicationId + "/precedence";
+            return Atlas.deleteRequest(url);
         }
         
     }
  })
-.factory('SourceRequests', function ($http, atlasHost) {
+.factory('SourceRequests', function (Atlas) {
     return {
         all: function() {
-            return $http.get(atlasHost + '/requests.json').then(function (results) {
+            return Atlas.getRequest('/requests.json').then(function (results) {
                 return results.data.source_requests});
         },
         send: function(sourceId, applicationId, applicationUrl, email, reason, usageType) {
-            var url = atlasHost + "/sources/" + sourceId + "/requests?" 
+            var url = "/sources/" + sourceId + "/requests?" 
                  + "appId=" + applicationId
                  + "&appUrl=" + encodeURIComponent(applicationUrl)
                  + "&email=" + encodeURIComponent(email)
                  + "&reason=" + encodeURIComponent(reason)
                  + "&usageType=" + usageType;
-            return $http.post(url, {});
+            return Atlas.postRequest(url, {});
         },
         approve: function(requestId) {
-            var url = atlasHost + '/requests/' + requestId + '/approve';
-            return $http.post(url, {}, {withCredentials: false});
+            var url = '/requests/' + requestId + '/approve';
+            return Atlas.postRequest(url, {});
         }
+    }
+})
+.factory('Atlas', function ($http, atlasHost) {
+    return {
+       getRequest: function(url) {
+           return $http.get(atlasHost + url);   
+       },
+       postRequest: function(url, data) {
+           return $http.post(atlasHost + url, data, {withCredentials: false});   
+       },
+       deleteRequest: function(url) {
+           return $http.delete(atlasHost + url);
+       }
     }
 });
