36d3a63b4a9576fd26c41d2d975c1645b4ec465f
liamgh <liam@greenhughes.com>
2014-03-10 10:29:02 +0000

    Create components for Stripe integration

---
+++ app/js/controllers/stripe.js
@@ -0,0 +1,8 @@
+'use strict';
+var app = angular.module('atlasAdmin.controllers.stripe', []);
+
+app.controller('CtrlStripe', function($scope, Users, Stripe, $log) { 
+    
+$scope.app = [];
+
+});
---
+++ app/js/services/stripe.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/* Services */
+var app = angular.module('atlasAdmin.services.stripe', []);
+app.factory('Stripe', function (Atlas, Applications, stripePublishableKey, $log) {
+    return {}
+});
