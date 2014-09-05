ad5777ddc1d0996f7c930919500ac50b5a9c5a6a
Jason Howmans <jason@metabroadcast.com>
2014-09-05 11:09:29 +0100

    refactoring: moved atlas request logic into provider

---
+++ atlas-admin-api/lib/services/atlasProvider.js
@@ -0,0 +1,48 @@
+var common = require('../../common'),
+    config = require('../../config'),
+    http   = require('http');
+
+var Atlas = function() {
+    'use strict';
+
+    var appendOauthToken = function(path) {
+        if (!common.oauth.token || !common.oauth.provider) return false;
+        var path = path || '',
+            prepend = (path.indexOf('?') > -1)? '&' : '?';
+        return path+prepend+'oauth_provider='+common.oauth.provider+'&oauth_token='+common.oauth.token;
+    }
+
+    var request = function(path, type, callback) {
+        var type = type || 'GET';
+        var path = path || '';
+        var opts = {
+            port: 80,
+            method: type,
+            host: config.atlasHost,
+            path: appendOauthToken('/4'+path),
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+
+        var request = http.request(opts, function(response) {
+            response.setEncoding('utf8');
+            var data = '',
+                status = response.statusCode;
+            response.on('data', function(chunk) {
+                data += chunk;
+            })
+            .on('end', function() { 
+                callback(status, data)
+            });
+        });
+        request.end();
+    }
+
+    return {
+        appendOauthToken: appendOauthToken,
+        request: request
+    }
+}
+
+module.exports = Atlas();
