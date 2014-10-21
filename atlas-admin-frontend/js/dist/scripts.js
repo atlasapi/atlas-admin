95a31ce2d3afda0910e457852a6a70635724beaf
Jason Howmans <jasonhowmans@me.com>
2014-10-21 20:00:39 +0100

    Graph is working

--- atlas-admin-frontend/js/dist/scripts.js
+++ atlas-admin-frontend/js/dist/scripts.js
@@ -2408,10 +2408,10 @@ app.controller('CtrlUsage', ['$scope', '$rootScope', 'APIUsage',
             width = 1000 - margin.left - margin.right,
             height = 400 - margin.top - margin.bottom;
 
-        var x = d3.scale.linear().domain([0, 400]).range([0, width]);
-        var y = d3.scale.linear().domain([0, 1400]).range([height, 0]);
+        var x = d3.scale.linear().domain([0, width]).range([0, width]);
+        var y = d3.scale.linear().domain([0, height]).range([height, 0]);
 
-        var req_per_min = d3.select('.rpm-chart-container').append('svg:svg')
+        var req_graph = d3.select('.rpm-chart-container').append('svg:svg')
                             .attr("width", width + margin.left + margin.right)
                             .attr("height", height + margin.top + margin.bottom)
                             .append('svg:g')
@@ -2425,7 +2425,24 @@ app.controller('CtrlUsage', ['$scope', '$rootScope', 'APIUsage',
                 return y(d.count); 
             })
 
-        req_per_min.append('svg:path').arrt('d', line(histogram));
+        // create yAxis
+        var xAxis = d3.svg.axis().scale(x).tickSize(-height).tickSubdivide(true);
+        // Add the x-axis.
+        req_graph.append("svg:g")
+              .attr("class", "x axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(xAxis);
+
+
+        // create left yAxis
+        var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+        // Add the y-axis to the left
+        req_graph.append("svg:g")
+              .attr("class", "y axis")
+              .attr("transform", "translate(-25,0)")
+              .call(yAxisLeft);
+
+        req_graph.append('svg:path').attr('d', line(histogram));
     });
     
 }]);
