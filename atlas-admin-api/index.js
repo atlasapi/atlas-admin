0c158397e8994ff90c680b5eb87164efe96aad7d
Jason Howmans <jasonhowmans@me.com>
2014-11-03 16:48:09 +0000

    Getting user groups from node api

--- atlas-admin-api/index.js
+++ atlas-admin-api/index.js
@@ -6,8 +6,6 @@ var config                       = require('./config'),
     MongoClient                  = require('mongodb').MongoClient,
     MongoServer                  = require('mongodb').Server,
     app                          = express(),
-    auth                         = require('./lib/middleware/auth'),
-    prepResponse                 = require('./lib/middleware/prepResponse'),
     gatewayRequests              = require('./lib/gateways/requests'),
     gatewayPropositions          = require('./lib/gateways/propositions'),
     gatewayUsage                 = require('./lib/gateways/usage'),
@@ -17,26 +15,19 @@ var _http_port = config.port.http;
 var _mongo_port = config.port.mongo;
 
 // middleware: parse incoming request data as json
-app.use(bodyParser.json());
+app.use( bodyParser.json() );
 
 // middleware: set content-type headers, and prep response
-app.use(prepResponse);
+app.use( require('./lib/middleware/prepResponse') );
 
 // middleware: check calls against whitelisted domains
-app.use(function allowCrossDomain(req, res, next) {
-    var origin = req.headers.origin;
-    if (config.allowedDomains.indexOf(origin) !== -1) {
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
-    }else{
-        res.header('401'); 
-    }
-    next();
-});
+app.use( require('./lib/middleware/allowAccess') );
 
 // middleware: proxy atlas requests
-app.use(auth);
+app.use( require('./lib/middleware/auth') );
+
+// middleware: user grouping
+app.use( require('./lib/middleware/userGroups').middleware );
 
 // open up a connection to mongodb, then register endpoints and boot the server
 var mongoclient = new MongoClient(
@@ -46,11 +37,14 @@ mongoclient.open(function(err, mongo) {
     if (err) console.error(err); 
     var db = mongo.db(config.database.name);
 
-    // register REST endpoints
-    app.use(config.paths.apiRoot + '/requests', gatewayRequests(db));
+    // register gateway REST endpoints
+    app.use(config.paths.apiRoot + '/requests',     gatewayRequests(db));
     app.use(config.paths.apiRoot + '/propositions', gatewayPropositions(db));
-    app.use(config.paths.apiRoot + '/wishes', gatewayWishes(db));
-    app.use(config.paths.apiRoot + '/usage', gatewayUsage(db));
+    app.use(config.paths.apiRoot + '/wishes',       gatewayWishes(db));
+    app.use(config.paths.apiRoot + '/usage',        gatewayUsage(db));
+
+    // return group info for current user
+    app.use(config.paths.apiRoot + '/groups', require('./lib/middleware/userGroups').query(db));
 
     // listen for requests to server on _http_port
     console.log('listen on port: ' + _http_port);
