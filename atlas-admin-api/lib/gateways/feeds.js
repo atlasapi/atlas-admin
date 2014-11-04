371d41bcaa1719884b0fe73a2c788c5d390cba84
Jason Howmans <jasonhowmans@me.com>
2014-11-04 09:51:34 +0000

    Proxting request through to atlas

--- atlas-admin-api/lib/gateways/feeds.js
+++ atlas-admin-api/lib/gateways/feeds.js
@@ -4,6 +4,7 @@ var config      = require('../../config'),
     express     = require('express'),
     http        = require('http'),
     Q           = require('q'),
+    qs          = require('querystring'),
     _           = require('lodash'),
     Atlas       = require('../services/atlasProvider');
 
@@ -13,12 +14,12 @@ var config      = require('../../config'),
 //  @returns promise
 //
 function proxyRequest(request) {
-    var defer = Q.defer(),
-        _apikey = '1a8c17b3ed3040aca8c30a46bd38e685',
-        _endpoint = request.params.endpoint,
-        _annotations = request.query.annotations || null;
-
-    Atlas.request('/3.0/feeds/youview/bbc_nitro/'+_endpoint+'.json?apiKey='+_apikey, 'GET', function(status, data) {
+    var defer           = Q.defer(),
+        _apikey         = '1a8c17b3ed3040aca8c30a46bd38e685',
+        _endpoint       = request.params.endpoint,
+        _annotations    = request.query.annotations || null,
+        query           = qs.stringify({ apiKey: _apikey, annotations: _annotations });
+    Atlas.request('/3.0/feeds/youview/bbc_nitro/'+_endpoint+'.json?'+query, 'GET', function(status, data) {
         defer.resolve(JSON.parse(data));
     });
     return defer.promise;
@@ -39,12 +40,10 @@ var feedsInterface = function() {
             res.end(JSON.stringify(_feeds));
         });
 
-    //  hardwired for now: catch the request to atlas so we can run 
+    //  hardwired for now, catch the request to atlas so we can run 
     //  auth checks before returning any data
     router.route('/youview/bbc_nitro/:endpoint.json')
         .get(function(req, res) {
-            var _annotations = req.query.annotations || null;
-
             proxyRequest(req).then(function(result) {
                 res.end(JSON.stringify(result));
             }, function(err) {
