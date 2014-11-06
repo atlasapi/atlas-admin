9377e427a65dac602726c296d2e6e7c64e329b9c
Jason Howmans <jasonhowmans@me.com>
2014-11-06 16:55:15 +0000

    User whitelisting

--- atlas-admin-frontend/js/controllers/userProfile.js
+++ atlas-admin-frontend/js/controllers/userProfile.js
@@ -101,6 +101,16 @@ app.controller('UserMenuController', function($scope, Users, $rootScope, Authent
             {path:'/manage/usage', label:'API Usage', role:'admin'},
             {path:'/manage/wishlist', label:'Wishlist', role:'admin'}];
 
+        // Add blackout widget page to navigation 
+        if (user.id === 'hk98' || 
+            user.id === 'hmbc' || 
+            user.id === 'hmjh' || 
+            user.id === 'hmjg' || 
+            user.id === 'hmjc' ||
+            user.id === 'hmbb') {
+            allMenu.push({path: '/epg/bt-tv', label: 'EPG'});
+        }
+
         var menu = [];
         var admin_menu = [];
         for (var i = 0; i < allMenu.length; i++) {
