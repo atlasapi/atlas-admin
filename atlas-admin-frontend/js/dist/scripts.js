918e1d0ac241afeb8dee7d973279b9a0f0cc6dee
Jason Howmans <jasonhowmans@me.com>
2014-11-11 15:58:26 +0000

    updated widget urls in stage

--- atlas-admin-frontend/js/dist/scripts.js
+++ atlas-admin-frontend/js/dist/scripts.js
@@ -1879,24 +1879,6 @@ app.controller('CtrlVideoSourceYouTubeConfig', function($scope, $rootScope, User
         });
     };
 });
-var app = angular.module('atlasAdmin.controllers.epgWidget', []);
-
-app.controller('CtrlEPGWidget', ['$scope', '$rootScope', 'Users', '$routeParams', '$q', '$http', 'Authentication', 'atlasApiHost',
-    function($scope, $rootScope, Users, $routeParams, $q, $http, Authentication, atlasApiHost) {
-    $scope.view_title = "";
-    $scope.widget = false;
-    $scope.widgetURL = '';
-
-    $http.get( Authentication.appendTokenToUrl(atlasApiHost +'/user/permissions') )
-    .success(function(groups, status) {
-        var key = groups[0].data.apiKey || null;
-        if (key) {
-            $scope.view_title = "BT Blackout";
-            $scope.widget = true;
-            $scope.widgetURL = '//widgets.metabroadcast.com/loader/1/btblackout.html?apiKey='+key;
-        }
-    })
-}]);
 'use strict';
 var app = angular.module('atlasAdmin.controllers.feeds', []);
 
