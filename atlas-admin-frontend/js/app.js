3ff3136e0116e26b696efb2ecb84d7bba11504ee
Jason Howmans <jasonhowmans@me.com>
2014-12-15 17:02:53 +0000

    Interceptors now live in their own directory. app.js is now lighter.

--- atlas-admin-frontend/js/app.js
+++ atlas-admin-frontend/js/app.js
@@ -2,49 +2,49 @@
 
 // Declare app level module which depends on filters, and services
 var app = angular.module('atlasAdmin', [
-                        'atlasAdmin.filters', 
-                        'atlasAdmin.preloader', 
-                        'atlasAdmin.services.auth',
-                        'atlasAdmin.services.atlas',
-                        'atlasAdmin.services.applications',
-                        'atlasAdmin.services.sources',
-                        'atlasAdmin.services.sourceRequests',
-                        'atlasAdmin.services.sourceLicenses',
-                        'atlasAdmin.services.users', 
-                        'atlasAdmin.services.uservideosources',
-                        'atlasAdmin.services.uservideosources.youtube',
-                        'atlasAdmin.services.propositions',
-                        'atlasAdmin.services.usage',
-                        'atlasAdmin.services.feeds',
-                        'atlasAdmin.services.bbcscrubbables',
-                        'atlasAdmin.directives.orderable', 
-                        'atlasAdmin.directives.focus',
-                        'atlasAdmin.directives.activePath',
-                        'atlasAdmin.directives.validUsage',
-                        'atlasAdmin.directives.inputmorph',
-                        'atlasAdmin.directives.loadContent',
-                        'atlasAdmin.directives.bbcscrubbables',
-                        'atlasAdmin.controllers.auth',
-                        'atlasAdmin.controllers.atlas',
-                        'atlasAdmin.controllers.errors',
-                        'atlasAdmin.controllers.applications',
-                        'atlasAdmin.controllers.wishlist',
-                        'atlasAdmin.controllers.sources',
-                        'atlasAdmin.controllers.epgwidget',
-                        'atlasAdmin.controllers.requestSource',
-                        'atlasAdmin.controllers.sourceRequests',
-                        'atlasAdmin.controllers.user',
-                        'atlasAdmin.controllers.feeds',
-                        'atlasAdmin.controllers.uservideosources',
-                        'atlasAdmin.controllers.uservideosources.youtube',
-                        'atlasAdmin.controllers.bbcscrubbables',
-                        'atlasAdmin.controllers.admins.usage',
-                        'atlasAdmin.controllers.admins.manageSourceRequests',
-                        'atlasAdmin.controllers.admins.manageWishlist',
-                        'ui.bootstrap',
-                        'ngResource',
-                        'ngRoute',
-                        'atlasAdminConfig']);
+                         'atlasAdmin.filters', 
+                         'atlasAdmin.preloader', 
+                         'atlasAdmin.services.auth',
+                         'atlasAdmin.services.atlas',
+                         'atlasAdmin.services.applications',
+                         'atlasAdmin.services.sources',
+                         'atlasAdmin.services.sourceRequests',
+                         'atlasAdmin.services.sourceLicenses',
+                         'atlasAdmin.services.users', 
+                         'atlasAdmin.services.uservideosources',
+                         'atlasAdmin.services.uservideosources.youtube',
+                         'atlasAdmin.services.propositions',
+                         'atlasAdmin.services.usage',
+                         'atlasAdmin.services.feeds',
+                         'atlasAdmin.services.bbcscrubbables',
+                         'atlasAdmin.directives.orderable', 
+                         'atlasAdmin.directives.focus',
+                         'atlasAdmin.directives.activePath',
+                         'atlasAdmin.directives.validUsage',
+                         'atlasAdmin.directives.inputmorph',
+                         'atlasAdmin.directives.loadContent',
+                         'atlasAdmin.directives.bbcscrubbables',
+                         'atlasAdmin.controllers.auth',
+                         'atlasAdmin.controllers.atlas',
+                         'atlasAdmin.controllers.errors',
+                         'atlasAdmin.controllers.applications',
+                         'atlasAdmin.controllers.wishlist',
+                         'atlasAdmin.controllers.sources',
+                         'atlasAdmin.controllers.epgwidget',
+                         'atlasAdmin.controllers.requestSource',
+                         'atlasAdmin.controllers.sourceRequests',
+                         'atlasAdmin.controllers.user',
+                         'atlasAdmin.controllers.feeds',
+                         'atlasAdmin.controllers.uservideosources',
+                         'atlasAdmin.controllers.uservideosources.youtube',
+                         'atlasAdmin.controllers.bbcscrubbables',
+                         'atlasAdmin.controllers.admins.usage',
+                         'atlasAdmin.controllers.admins.manageSourceRequests',
+                         'atlasAdmin.controllers.admins.manageWishlist',
+                         'ui.bootstrap',
+                         'ngResource',
+                         'ngRoute',
+                         'atlasAdminConfig']);
 
 
 app.config(['$routeProvider', function($routeProvider) {
@@ -81,82 +81,4 @@ app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/logout', {templateUrl: 'partials/logout.html', controller: 'CtrlLogout'});
     $routeProvider.when('/error', {templateUrl: 'partials/error.html', controller: 'ErrorController', reloadOnSearch: false});
     $routeProvider.otherwise({redirectTo: '/applications'});
-  }])
-
-
-app.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-
-    // these are used for intercepting the request and running checks 
-    // for authentication and profile complete-ness
-    $httpProvider.responseInterceptors.push('AuthenticationInterceptor');
-    $httpProvider.responseInterceptors.push('ProfileCompleteInterceptor');
-
-    // the loading bar will show when there are requests still pending, but will
-    // be delayed slighty so we dont see the loader flashing up for pages that
-    // load quickly. This also gives the illusion of faster page loads
-    $httpProvider.interceptors.push(['$q', '$rootScope', '$injector', '$timeout', '$location',
-        function($q, $rootScope, $injector, $timeout, $location) {
-        var requests = 0;
-        var loadTimer;
-        var restrictedLocations = ['scrubbables'];
-
-        if (!$rootScope.show) {
-            $rootScope.show = {};
-        }
-
-        var restricted = function() {
-            var _path = $location.path();
-            for (var i in restrictedLocations) {
-                if (_path.indexOf(restrictedLocations[i]) > -1) return true;
-            }
-            return false;
-        }
-
-        var startLoading = function() {
-            var _loggedin = $rootScope.status.loggedIn || false;
-            if (requests > 1 && _loggedin && !restricted()) {
-                $timeout.cancel(loadTimer);
-                loadTimer = $timeout(function() {
-                    $rootScope.show.load = true;
-                    $rootScope.$broadcast('loading-started');
-                }, 400);
-            }
-        }
-
-        var endLoading = function() {
-            $timeout.cancel(loadTimer);
-            $rootScope.$broadcast('loading-complete');
-            $rootScope.show.load = false;
-        }
-
-        return {
-            'request': function(config) {
-                requests++;
-                startLoading();
-                return config || $q.when(config);
-            },
-            'response': function(response) {
-                requests--;
-                if (!requests) endLoading();
-                return response || $q.when(response);
-            }
-        };
-    }]);
-}]);
-
-
-
-// This is used for telling angular to allow transposing of strings
-// to make url's in the $scope
-app.config(['$sceDelegateProvider', function($sceDelegateProvider) {
-    $sceDelegateProvider.resourceUrlWhitelist([
-        'self',
-        'http://*.metabroadcast.com/**'
-        ]);
-}]);
-
-app.config(['$locationProvider', function($locationProvider) {
-    //$locationProvider.html5Mode(false).hashPrefix('!');
-}]);
+  }]);
