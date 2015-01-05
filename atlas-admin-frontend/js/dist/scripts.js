b0b92c02c5fe67e8b870845e227d3e9c013f106e
Jason Howmans <jasonhowmans@me.com>
2015-01-05 11:12:00 +0000

    Fixed bug where 403 would redirect the whole app instead of just logging the error

--- atlas-admin-frontend/js/dist/scripts.js
+++ atlas-admin-frontend/js/dist/scripts.js
@@ -6917,15 +6917,13 @@ app.factory('AuthenticationInterceptor', ['$q', '$location', 'atlasHost', 'atlas
 
         'responseError': function(response) {
             var _url = response.config.url;
-            // For redirecting to the login screen if api calls come back as bad
             if (_url.indexOf(atlasHost) !== -1 || _url.indexOf(atlasApiHost) !== -1) {
                 if (response.status === 400) {
                     console.error('Account not authenticated to make request to: '+_url);
                     $location.path('/login');
                 }else if (response.status === 403) {
-                    $window.location.href = '#/error?type=forbidden';
+                    console.error('You do not have access to the resource ' + _url);
                 }
-                return response || $q.defer(response);
             }
             return response || $q.defer(response);
         } 
