af684edaa0117e18f3ea876754037d61bebf7af3
Steve Rydz <steverydz@gmail.com>
2015-06-09 17:21:04 +0100

    Dummy data bar chart

--- atlas-admin-frontend/js/controllers/applications/applicationEdit.js
+++ atlas-admin-frontend/js/controllers/applications/applicationEdit.js
@@ -126,117 +126,83 @@ angular.module('atlasAdmin.controllers.applications')
     };
 
     var makeGraph = function (data) {
-        console.log(data);
+        // var barData = data.facets[0].entries;
+        // console.log(barData);
 
-        var barData = data.facets[0].entries;
+        var barData = [{
+            'x': 1,
+            'y': 5
+        }, {
+            'x': 20,
+            'y': 20
+        }, {
+            'x': 40,
+            'y': 10
+        }, {
+            'x': 60,
+            'y': 40
+        }, {
+            'x': 80,
+            'y': 5
+        }, {
+            'x': 100,
+            'y': 60
+        }];
 
-        var vis = d3.select('.usage-graph'),
-            WIDTH = 1000,
-            HEIGHT = 500,
-            MARGINS = {
-                top: 20,
-                right: 20,
-                bottom: 20,
-                left: 50
-            },
-            xRange = d3.scale.ordinal()
-                .rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1)
-                .domain(barData.map(function (d) {
-                    return d.count;
-            })),
-            yRange = d3.scale.linear().
-                range([HEIGHT - MARGINS.top, MARGINS.bottom])
-                .domain([0, d3.max(barData, function(d) {
-                    return d.time;
-            })]),
-            xAxis = d3.svg.axis()
-                .scale(xRange)
-                .tickSize(5)
-                .tickSubdivide(true),
-            yAxis = d3.svg.axis()
-                .scale(yRange)
-                .tickSize(5)
-                .orient('left')
-                .tickSubdivide(true);
+        var vis = d3.select('#visualisation'),
+        WIDTH = 1000,
+        HEIGHT = 500,
+        MARGINS = {
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 50
+      },
+      xRange = d3.scale.ordinal().rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1).domain(barData.map(function(d) {
+  return d.x;
+})),
 
-        vis.append('svg:g')
-            .attr('class', 'x axis')
-            .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
-            .call(xAxis);
-         
-        vis.append('svg:g')
-            .attr('class', 'y axis')
-            .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
-            .call(yAxis);
+      yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0,
+  d3.max(barData, function(d) {
+    return d.y;
+  })]),
 
-        vis.selectAll('rect')
-            .data(barData)
-            .enter()
-            .append('rect')
-            .attr('x', function (d) { // sets the x position of the bar
-                return xRange(d.count);
-            })
-            .attr('y', function (d) { // sets the y position of the bar
-                return yRange(d.time);
-            })
-            .attr('width', '20px') // sets the width of bar
-            .attr('height', function (d) {      // sets the height of bar
-                return d.count + 'px';
-            })
-            .attr('fill', 'grey');   // fills the bar with grey color
-        // var vis = d3.select('.usage-graph');
-        // var WIDTH = 1000;
-        // var HEIGHT = 500;
-        // var MARGINS = {
-        //     top: 20,
-        //     right: 20,
-        //     bottom: 20,
-        //     left: 50
-        // };
-        // var xRange = d3.scale.ordinal()
-        //     .rangeRoundBands([MARGINS.left, WIDTH - MARGINS.right], 0.1)
-        //     .domain(barData.map(function(d) {
-        //         return d.count;
-        //     }));
-        // var yRange = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom])
-        //     .domain([0, d3.max(barData, function(d) {
-        //         return d.time;
-        //     })]);
-        // var xAxis = d3.svg.axis()
-        //     .scale(xRange)
-        //     .tickSize(5)
-        //     .tickSubdivide(true);
-        // var yAxis = d3.svg.axis()
-        //     .scale(yRange)
-        //     .tickSize(5)
-        //     .orient('left')
-        //     .tickSubdivide(true);
+      xAxis = d3.svg.axis()
+      .scale(xRange)
+      .tickSize(5)
+      .tickSubdivide(true),
 
-        // vis.append('svg:g')
-        //     .attr('class', 'x axis')
-        //     .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
-        //     .call(xAxis);
+      yAxis = d3.svg.axis()
+      .scale(yRange)
+      .tickSize(5)
+      .orient("left")
+      .tickSubdivide(true);
 
-        // vis.append('svg:g')
-        //     .attr('class', 'y axis')
-        //     .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
-        //     .call(yAxis);
+      vis.append('svg:g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')')
+      .call(xAxis);
 
-        // vis.selectAll('rect')
-        //     .data(barData)
-        //     .enter()
-        //     .append('rect')
-        //     .attr('x', function (d) {
-        //         return xRange(d.count);
-        //     })
-        //     .attr('y', function (d) {
-        //         return yRange(d.time);
-        //     })
-        //     .attr('width', xRange.rangeBand())
-        //     .attr('height', function (d) {
-        //         return ((HEIGHT - MARGINS.bottom) - yRange(d.time));
-        //     })
-        //     .attr('fill', 'grey');
+      vis.append('svg:g')
+      .attr('class', 'y axis')
+      .attr('transform', 'translate(' + (MARGINS.left) + ',0)')
+      .call(yAxis);
+
+      vis.selectAll('rect')
+  .data(barData)
+  .enter()
+  .append('rect')
+  .attr('x', function(d) { // sets the x position of the bar
+    return xRange(d.x);
+  })
+  .attr('y', function(d) { // sets the y position of the bar
+    return yRange(d.y);
+  })
+  .attr('width', xRange.rangeBand()) // sets the width of bar
+  .attr('height', function(d) {      // sets the height of bar
+    return ((HEIGHT - MARGINS.bottom) - yRange(d.y));
+  })
+  .attr('fill', 'grey');   // fills the bar with grey color
     };
 
     var loadGraphHour = function (apiKey) {
