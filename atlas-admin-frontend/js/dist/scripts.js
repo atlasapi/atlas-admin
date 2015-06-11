39054fd1ab5358bc010979583623a760c2b5a33b
Steve Rydz <steverydz@gmail.com>
2015-06-11 17:39:33 +0100

    Make dates for usage table dynamic

--- atlas-admin-frontend/js/dist/scripts.js
+++ atlas-admin-frontend/js/dist/scripts.js
@@ -14533,9 +14533,16 @@ var app = angular.module('atlasAdmin.controllers.admins.usage', []);
 
 app.controller('CtrlUsage', ['$scope', '$rootScope', 'Authentication', 'atlasApiHost', '$http', function($scope, $rootScope, Authentication, atlasApiHost, $http) {
   var getUsageData = function () {
-    var timePeriod = 'logstash-atlas-access-2015.06.09,logstash-atlas-access-2015.06.08,logstash-atlas-access-2015.06.07,logstash-atlas-access-2015.06.06,logstash-atlas-access-2015.06.05,logstash-atlas-access-2015.06.04,logstash-atlas-access-2015.06.03,logstash-atlas-access-2015.06.02';
+    var TIME_PERIOD = 8;
+    var dates = [];
 
-    $http.get(Authentication.appendTokenToUrl(atlasApiHost + '/usage-list/' + timePeriod)).then(function (response) {
+    for (var i = 0, ii = TIME_PERIOD; i < ii; i++) {
+      dates.push('logstash-atlas-access-' + moment().subtract(i, 'days').format('YYYY.MM.DD'));
+    }
+
+    dates = dates.join(',');
+
+    $http.get(Authentication.appendTokenToUrl(atlasApiHost + '/usage-list/' + dates)).then(function (response) {
       $scope.requests = response.data.aggregations.apiKeys.buckets;
     });
   };
