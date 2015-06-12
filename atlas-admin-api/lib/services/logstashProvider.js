85ec098b332056bdfc5f7ffda55bcf4b7129b06e
Steve Rydz <steverydz@gmail.com>
2015-06-12 15:10:27 +0100

    Change XGET to GET

--- atlas-admin-api/lib/services/logstashProvider.js
+++ atlas-admin-api/lib/services/logstashProvider.js
@@ -116,7 +116,7 @@ function Logstash() {
             hostname: _logstash_host,
             port: 9200,
             path: '/_search',
-            method: 'XGET'
+            method: 'GET'
         }
 
         var req = http.request(_opts, function(response) {
