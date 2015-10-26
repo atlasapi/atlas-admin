e13562656a584b34f24f8acc4f8423528c99afb3
Jason Howmans <jason@metabroadcast.com>
2015-10-26 15:03:04 +0000

    Refactor of the atlasSearch directive

--- atlas-admin-frontend/js/directives/atlasSearch.js
+++ atlas-admin-frontend/js/directives/atlasSearch.js
@@ -1,105 +1,108 @@
 var app = angular.module('atlasAdmin.directives.bbcscrubbables');
 
 app.directive('atlasSearch', ['$document', '$q', '$timeout', 'atlasHost', '$http', 'GroupsService', 'BBCScrubbablesService', 'ScrubbablesHelpers', '$location',
-    function($document, $q, $timeout, atlasHost, $http, Groups, Scrubbables, Helpers, $location) {
-
-    var controller = function($scope, $el, $attr) {
-        var $el = $($el);
-        var input_timer;
-        $scope.atlasSearch = {};
-        $scope.atlasSearch.selectedItem = {};
+function($document, $q, $timeout, atlasHost, $http, Groups, Scrubbables, Helpers, $location) {
+  
+  var controller = function($scope, $el) {
+    $el = $($el);
+    var input_timer;
+    $scope.atlasSearch = {};
+    $scope.atlasSearch.selectedItem = {};
+    $scope.atlasSearch.showAutocomplete = false;
+    
+    $scope.atlasSearch.selectAtlasItem = function(title, id) {
+      if (!_.isString(title) && !_.isString(id)) { 
+        return false;
+      }
+      $location.path('/scrubbables/'+id);
+      $scope.loading = true;
+      $scope.atlasSearch.searchquery = title;
+      $scope.atlasSearch.showAutocomplete = false;
+    };
+    
+    $scope.atlasSearch.messageOutput = function(message) {
+      $scope.atlasSearch.showMessage = (typeof message === 'string') ? true : false;
+      if ($scope.atlasSearch.showMessage) {
+        $scope.atlasSearch.message = message;
+        $scope.atlasSearch.showMessage = true;
         $scope.atlasSearch.showAutocomplete = false;
-
-        $scope.atlasSearch.selectAtlasItem = function(title, id) {
-            if (!_.isString(title) && !_.isString(id)) { 
-                return false;
-            }
-            var _result;
-            $location.path('/scrubbables/'+id);
-            $scope.loading = true;
-            $scope.atlasSearch.searchquery = title;
-            $scope.atlasSearch.showAutocomplete = false;
+      }else{
+        $scope.atlasSearch.message = '';
+        $scope.atlasSearch.showMessage = false;
+      }
+    };
+    
+    var searchRequest = function() {
+      var _query = $scope.atlasSearch.searchquery;
+      if (!_query.length) {
+        return;
+      }
+      
+      var getContentForUri = function(uri) {
+        var defer = $q.defer();
+        if (!_.isString(uri)) {
+          defer.reject(new Error('URI arg should be a string'));
+          return defer.promise;
         }
-
-        $scope.atlasSearch.messageOutput = function(message) {
-            $scope.atlasSearch.showMessage = (typeof message === 'string') ? true : false;
-            var _messagetpl;
-            if ($scope.atlasSearch.showMessage) {
-                $scope.atlasSearch.message = message;
-                $scope.atlasSearch.showMessage = true;
-                $scope.atlasSearch.showAutocomplete = false;
+        Scrubbables.content.uri($scope.searchKey, uri).then(function(item) {
+          defer.resolve(item.contents[0]);
+        });
+        return defer.promise;
+      };
+      
+      Scrubbables.search($scope.searchKey, _query).then(function(res) {
+        var broadcasts;
+        broadcasts = res.contents[0].broadcasts || null;
+        if (broadcasts) {
+          broadcasts = _.filter(res.contents[0].broadcasts, function(bcast) {
+            if (bcast.channel.id === 'cbbh') {
+              return true;
+            }
+          });
+          if (broadcasts.length) {
+            getContentForUri(res.contents[0].uri).then(
+              function(contents) {
+                $scope.atlasSearch.searchResults.push( Helpers.formatResponse(contents) );
+                $scope.atlasSearch.messageOutput(null);
+                $scope.atlasSearch.showAutocomplete = true;
+              }, console.error);
             }else{
-                $scope.atlasSearch.message = '';
-                $scope.atlasSearch.showMessage = false;
+              $scope.atlasSearch.messageOutput('No results found');
+              $scope.atlasSearch.showAutocomplete = false;
             }
+          }else{
+            $scope.atlasSearch.messageOutput('No results found');
+            $scope.atlasSearch.showAutocomplete = false;
+          }
+        }, function(err) {
+          console.error(err);
+          $scope.atlasSearch.showAutocomplete = false;
+        });
+      };
+      
+      $scope.atlasSearch.lookupAtlasItem = function() {
+        var _query = $scope.atlasSearch.searchquery;
+        $scope.atlasSearch.message = null;
+        $scope.atlasSearch.searchResults = [];
+        if (!_.isString(_query)) {
+          return;
         }
-
-        var searchRequest = function() {
-            var _query = $scope.atlasSearch.searchquery;
-            if (!_query.length) return;
-
-            var getContentForUri = function(uri) {
-                var bbcone;
-                var defer = $q.defer();
-                if (!_.isString(uri)) {
-                    defer.reject(new Error('URI arg should be a string'));
-                    return defer.promise;
-                }
-                Scrubbables.content.uri($scope.searchKey, uri).then(function(item) {
-                    defer.resolve(item.contents[0]);
-                });
-                return defer.promise;
-            }
-
-            Scrubbables.search($scope.searchKey, _query).then(function(res) {
-                var broadcasts, air_date;
-                broadcasts = res.contents[0].broadcasts || null;
-                if (broadcasts) {
-                    broadcasts = _.filter(res.contents[0].broadcasts, function(bcast) {
-                        if (bcast.channel.id === 'cbbh') return true;
-                    })
-                    if (broadcasts.length) {
-                        getContentForUri(res.contents[0].uri).then(
-                            function(contents) {
-                            $scope.atlasSearch.searchResults.push( Helpers.formatResponse(contents) );
-                            $scope.atlasSearch.messageOutput(null);
-                            $scope.atlasSearch.showAutocomplete = true;
-                        }, function(err) { console.error(err) });
-                    }else{
-                        $scope.atlasSearch.messageOutput('No results found');
-                        $scope.atlasSearch.showAutocomplete = false;
-                    }
-                }else{
-                    $scope.atlasSearch.messageOutput('No results found');
-                    $scope.atlasSearch.showAutocomplete = false;
-                }
-            }, function(err) {
-                $scope.atlasSearch.showAutocomplete = false;
-            })
+        if (!_query.length) {
+          $timeout.cancel(input_timer);
+          $scope.atlasSearch.search_results = null;
+          $scope.atlasSearch.showAutocomplete = false;
+        } else if (_query.length > 2) {
+          $scope.atlasSearch.messageOutput('Searching...');
+          $timeout.cancel(input_timer);
+          input_timer = $timeout(searchRequest, 1000);
         }
-
-        $scope.atlasSearch.lookupAtlasItem = function() {
-            var _query = $scope.atlasSearch.searchquery;
-            $scope.atlasSearch.message = null;
-            $scope.atlasSearch.searchResults = [];
-            if (!_.isString(_query)) return;
-            if (!_query.length) {
-                $timeout.cancel(input_timer);
-                $scope.atlasSearch.search_results = null;
-                $scope.atlasSearch.showAutocomplete = false;
-            } else if (_query.length > 2) {
-                $scope.atlasSearch.messageOutput('Searching...');
-                $timeout.cancel(input_timer);
-                input_timer = $timeout(searchRequest, 1000);
-            }
-        }
-
-    }
-
+      };
+    };
+    
     return {
-        restrict: 'E',
-        scope: false,
-        link: controller,
-        templateUrl: 'partials/bbcScrubbables/atlasSearch.html'
-    }
-}]);
+      restrict: 'E',
+      scope: false,
+      link: controller,
+      templateUrl: 'partials/bbcScrubbables/atlasSearch.html'
+    };
+  }]);
