d22f01739c01b79a6dc6ea383502ff3b340e4a92
Steve Rydz <steverydz@gmail.com>
2015-12-18 17:19:59 +0000

    Update hostname

--- atlas-admin-api/config/index.js
+++ atlas-admin-api/config/index.js
@@ -9,6 +9,7 @@ module.exports = (function() {
     return {
         atlasHost: instanceConfig.atlasHost || 'stage.atlas.metabroadcast.com',
         processingHost: instanceConfig.processingHost || 'processing.stage.atlas.mbst.tv',
+        processingYouViewHost: instanceConfig.prpcessingYouViewHost || 'processing-youview.stage.atlas.mbst.tv',
 
         port: {
             http: instanceConfig.listenPort || 9000
