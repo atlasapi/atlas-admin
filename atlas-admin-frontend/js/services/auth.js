a05247b16945107e8f557a218acfaf08c765652a
Steve Rydz <steverydz@gmail.com>
2016-01-05 17:27:22 +0000

    Adding auth

--- atlas-admin-frontend/js/services/auth.js
+++ atlas-admin-frontend/js/services/auth.js
@@ -1,8 +1,8 @@
 'use strict';
 var app = angular.module('atlasAdmin.services.auth', []);
 
-app.factory('Authentication', ['$rootScope', 'ProfileStatus',
-    function ($rootScope, ProfileStatus) {
+app.factory('Authentication', ['$rootScope', 'ProfileStatus', 'userUrl',
+    function ($rootScope, ProfileStatus, userUrl) {
     if (!$rootScope.status) {
         $rootScope.status = {};
     }
@@ -30,10 +30,29 @@ app.factory('Authentication', ['$rootScope', 'ProfileStatus',
             var provider = localStorage.getItem('auth.provider');
             var token = localStorage.getItem('auth.token');
             var oauthParams = 'oauth_provider=' + provider + '&oauth_token=' + token;
+            var userCookie = Cookies.get('iPlanetDirectoryPro');
+            var options = {
+              url: userUrl,
+              headers: {
+                iPlanetDirectoryPro: userCookie
+              }
+            };
+
+            userMigration.isUserLoggedIn(options, function (response) {
+              if (!response) {
+                $rootScope.status.loggedIn = false;
+                return;
+              }
+
+              $rootScope.status.loggedIn = true;
+            });
+
             if (!token) {
                 return url;
             }
+
             $rootScope.status.loggedIn = true;
+
             return (url.indexOf('?') === -1) ?
                         url + '?' + oauthParams :
                         url + '&' + oauthParams;
