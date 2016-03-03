ee2262ddb539bc183f0a3f4c0749c9a8e89e4db4
Phil Giles <phil@metabroadcast.com>
2016-03-03 16:53:13 +0000

    updated login tests

--- atlas-admin-frontend/tests/login/login.e2e.js
+++ atlas-admin-frontend/tests/login/login.e2e.js
@@ -1,24 +1,24 @@
 'use strict';
 
-describe('E2E applications', function () {
+describe('E2E login', function () {
   var loginPageObject = require('./login.pageobject.js');
 
   it('should load the login view', function () {
     loginPageObject.get();
-    var currentLocation = loginPageObject.getCurrentLocation();
-    expect(currentLocation).toBe('/login');
+    expect(loginPageObject.getCurrentLocation()).toBe('/login');
   });
 
   it('should redirect to twitter login', function () {
-    browser.ignoreSynchronization = true;
-    $('.auth-option.twitter').click();
-    expect(browser.getCurrentUrl()).toContain('https://api.twitter.com/oauth/authenticate');
+    $('.auth-option.twitter').click().then(function () {
+      expect(browser.getCurrentUrl()).toContain('https://api.twitter.com/oauth/authenticate');
+    });
   });
 
   it('should login to twitter account then redirect to the app', function () {
-    var auth = require('../../protractor.login.conf.js');
+    var auth = require('./login.conf.js');
     var protractor = require('protractor');
     var driver = browser.driver;
+    protractor.ignoreSynchronization = true;
 
     var findById = function(name) {
       return driver.findElement(protractor.By.id(name));
@@ -27,12 +27,7 @@ describe('E2E applications', function () {
     findById('username_or_email').sendKeys(auth.user);
     findById('password').sendKeys(auth.pass);
     findById('allow').click();
-  });
 
-  it('should redirect to the app', function () {
-    browser.ignoreSynchronization = false;
-    browser.wait(protractor.until.elementIsVisible(".application-container"), 5000, 'Error: Element did not display within 5 seconds');
-    var currentLocation = loginPageObject.getCurrentLocation();
-    expect(currentLocation).toBe('/applications');
+    driver.sleep(2000);
   });
 });
