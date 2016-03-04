9bf4f6e4fab379609f50d8c7197b6b71c248811a
Phil Giles <phil@metabroadcast.com>
2016-03-04 09:57:03 +0000

    completed wishlist test

--- atlas-admin-frontend/tests/wishlist/wishlist.e2e.js
+++ atlas-admin-frontend/tests/wishlist/wishlist.e2e.js
@@ -1,5 +1,24 @@
 'use strict';
 
-describe('E2E wishlist', function () {
-  var wishlistPageObject = require('./wishlist.pageobject.js');
+var wishlistPageObject = require('./wishlist.pageobject.js');
+
+describe('E2E wishlist: Load', function () {
+  beforeEach(function () {
+    wishlistPageObject.get();
+  });
+  it('should load the wishlist view', function () {
+    expect(wishlistPageObject.getCurrentLocation()).toBe('/wishlist');
+    expect(wishlistPageObject.getH1()).toBe('Request new sources and features');
+  });
+});
+
+describe('E2E wishlist: Tabs', function () {
+  beforeEach(function () {
+    wishlistPageObject.get();
+  });
+  it('should switch tab', function () {
+    expect(wishlistPageObject.panelTitle()).toBe('Sources you\'ve asked for');
+    element(by.css('.app-tab.features')).click();
+    expect(wishlistPageObject.panelTitle()).toEqual('Features in the pipeline');
+  });
 });
