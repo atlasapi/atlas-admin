05a61cd1f63035ae73c0922a47cded0edce78d92
Phil Giles <phil@metabroadcast.com>
2016-03-07 10:25:57 +0000

    feeds tests complete

--- atlas-admin-frontend/tests/feeds/feeds.pageobject.js
+++ atlas-admin-frontend/tests/feeds/feeds.pageobject.js
@@ -7,4 +7,8 @@ feedsPageObject.get = function () {
   browser.get('#/feeds/youview');
 };
 
+feedsPageObject.getFeed = function () {
+  browser.get('#/feeds/youview/dxh4jj');
+};
+
 module.exports = feedsPageObject;
