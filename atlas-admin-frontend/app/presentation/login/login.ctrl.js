8ae756b9ef4b9bda29e9efbe5227d956e6ac42bf
Steve Rydz <steverydz@gmail.com>
2016-03-16 12:22:54 +0000

    Give option to login with openam

--- atlas-admin-frontend/app/presentation/login/login.ctrl.js
+++ atlas-admin-frontend/app/presentation/login/login.ctrl.js
@@ -1,45 +1,17 @@
 'use strict';
-angular.module('atlasAdmin.login')
-  .controller('CtrlLogin', function($scope, $rootScope, $rootElement, $routeParams, Atlas, atlasVersion, $location, Authentication, $log) {
-    $scope.title = "Hi there, please sign in to continue";
 
-    // Ask atlas for access here
-    Authentication.reset();
-    Atlas.getAuthProviders().then(function(results) {
-        var providers = [];
-        for (var i=0; i<results.length; i++) {
-            var provider = results[i];
-            provider.icon = (provider.namespace === 'google')? 'google-plus' : provider.namespace;
-            providers.push(provider);
-        }
-        $scope.providers = providers;
-        if ($routeParams.providerNamespace) {
-            $rootScope.startAuth($scope.providers.filter(function (provider) {
-                return provider.namespace === $routeParams.providerNamespace;
-            })[0]);
-        }
-    });
+angular
+  .module('atlasAdmin.login')
+  .controller('CtrlLogin', CtrlLogin);
 
-    $rootScope.startAuth = function(provider) {
-        var uri,
-            target;
-        if ($location.absUrl().indexOf('/login/' + provider.namespace) !== -1) {
-            uri = $location.absUrl().replace("/login/" + provider.namespace,"/oauth/" + provider.namespace);
-            target = $location.absUrl().replace("/login/" + provider.namespace,"/");
-        } else {
-            uri = $location.absUrl().replace("/login", "/oauth/" + provider.namespace);
-            target = $location.absUrl().replace("/login","/");
-        }
+CtrlLogin.$inject = ['$scope', '$location', '$window'];
 
-        var callbackUrl = encodeURIComponent(uri);
-        var targetUri = encodeURIComponent(target);
+function CtrlLogin($scope, $location, $window) {
+  $scope.title = "Hi there, please sign in to continue";
 
-        Authentication.setProvider(provider.namespace);
-        Atlas.startOauthAuthentication(provider, callbackUrl, targetUri).then(function(login_url) {
-            window.location.href = login_url;
-        }, function(error) {
-            $log.error("Error starting auth:");
-            $log.error(error);
-        });
-    };
-  });
+  var userServiceUrl = 'https://users.metabroadcast.com#login?ref=' + $location.absUrl();
+
+  $scope.goToUserService = function () {
+    $window.location = userServiceUrl;
+  };
+}
