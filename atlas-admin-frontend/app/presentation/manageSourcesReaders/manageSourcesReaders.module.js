b15ee262a0586db43fd24b213b053fb9d0f048f3
Phil Giles <phil@metabroadcast.com>
2016-03-16 09:41:09 +0000

    changed some table styles moved manage sources to one module

--- atlas-admin-frontend/app/presentation/manageSourcesReaders/manageSourcesReaders.module.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('atlasAdmin.manageSourcesReaders', ['ngRoute'])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/manage/sources/:sourceId/readers', {
-      templateUrl: 'presentation/manageSourcesReaders/manageSourcesReaders.tpl.html',
-      controller: 'CtrlSourceReaders'
-    });
-  }]);
--- atlas-admin-frontend/app/presentation/manageSourcesWriters/manageSourcesWriters.module.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('atlasAdmin.manageSourcesWriters', ['ngRoute', 'atlasAdmin.directives.focus'])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/manage/sources/:sourceId/writers', {
-      templateUrl: 'presentation/manageSourcesWriters/manageSourcesWriters.tpl.html',
-      controller: 'CtrlSourceWriters'
-    });
-  }]);
