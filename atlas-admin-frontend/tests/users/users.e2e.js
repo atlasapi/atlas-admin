65d8aace037f81def98b34c5d28690b8036d30d1
Phil Giles <phil@metabroadcast.com>
2016-03-21 15:37:18 +0000

    fixed broken tests

--- atlas-admin-frontend/tests/users/users.e2e.js
+++ atlas-admin-frontend/tests/users/users.e2e.js
@@ -15,7 +15,7 @@ describe('E2E users: Load', function () {
 
 describe('E2E users: filter', function () {
   it('should filters to singe user', function () {
-    element(by.css('.query-filter input')).sendKeys('phil giles');
+    element(by.css('.tbl-filters input')).sendKeys('phil giles');
     element.all(by.repeater('user in app.users | filter:query | orderBy:app.predicate:app.reverse | startFrom:(app.currentPage-1)*app.pageSize | limitTo:app.pageSize')).count().then(function(count) {
       expect(count).toEqual(1);
     });
