b0cb5cbef4ab2433e946e8c5fe5442905686527e
Steve Rydz <steverydz@gmail.com>
2016-03-21 11:17:55 +0000

    Formatting

--- atlas-admin-frontend/app/presentation/applications/applications.ctrl.js
+++ atlas-admin-frontend/app/presentation/applications/applications.ctrl.js
@@ -15,72 +15,83 @@ function CtrlApplications($scope, $rootScope, $routeParams, Applications, $uibMo
   $scope.app.currentPage = 1;
   $scope.isAdmin = false;
 
-  Atlas.getRequest('/auth/user.json').then(function (result) {
-    if (result.data.user.role === 'admin') {
-      $scope.isAdmin = true;
-    }
-  });
+  function httpSuccess(response) {
+    var applications = response.data.role;
+    $scope.app.applications = [];
 
-  var getUsageData = function (applications) {
-    var TIME_PERIOD = 8;
-    var dates = [];
-
-    for (var i = 0, ii = TIME_PERIOD; i < ii; i++) {
-      dates.push('logstash-atlas-access-' + moment().subtract(i, 'days').format('YYYY.MM.DD'));
-    }
-
-    dates = dates.join(',');
-
-    var numberWithCommas = function (x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    };
-
-    $http
-      .get(Authentication
-      .appendTokenToUrl(atlasApiHost + '/usage-list/' + dates))
-      .then(function (response) {
-        var usageData =  _.has(response, 'data') ? response.data.aggregations.apiKeys.buckets : null;
-          if (! usageData) {
-            console.warn('Response data doesnt have the `data` property', response);
-            return;
-          }
-
-          _.forEach(usageData, function (d) {
-            d.readableCount = numberWithCommas(d.doc_count);
-          });
-
-          applications = handleNullUsage(applications);
-          mapUsageDataToApplications(applications, usageData);
+    applications.forEach(function(application) {
+      Atlas
+        .getRequest('http://admin-backend.metabroadcast.com/1/applications/' + application.id)
+        .then(function(response) {
+          $scope.app.applications.push(response.data.application);
+          $scope.state = (applications.length) ? 'table' : 'blank';
+        })
+        .catch(httpError);
     });
-  };
+  }
 
-  var handleNullUsage = function (applications) {
-    _.forEach(applications, function (application) {
-      application.usage = {
-        doc_count: 0,
-        readableCount: 0
-      };
-    });
+  function httpError(error) {
+    console.error('error', error);
+  }
 
-    return applications;
-  };
+  Atlas
+    .getRequest('http://admin-backend.metabroadcast.com/1/user')
+    .then(httpSuccess)
+    .catch(httpError);
 
-  var mapUsageDataToApplications = function (applications, usageData) {
-    applications = _.map(applications, function (application) {
-      _.forEach(usageData, function (d) {
-        if (application.credentials.apiKey === d.key) {
-          application.usage = d;
-        }
-      });
-    });
-  };
+  // var getUsageData = function (applications) {
+  //   var TIME_PERIOD = 8;
+  //   var dates = [];
+  //
+  //   for (var i = 0, ii = TIME_PERIOD; i < ii; i++) {
+  //     dates.push('logstash-atlas-access-' + moment().subtract(i, 'days').format('YYYY.MM.DD'));
+  //   }
+  //
+  //   dates = dates.join(',');
+  //
+  //   var numberWithCommas = function (x) {
+  //     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  //   };
+  //
+  //   $http
+  //     .get(Authentication
+  //     .appendTokenToUrl(atlasApiHost + '/usage-list/' + dates))
+  //     .then(function (response) {
+  //       var usageData =  _.has(response, 'data') ? response.data.aggregations.apiKeys.buckets : null;
+  //         if (! usageData) {
+  //           console.warn('Response data doesnt have the `data` property', response);
+  //           return;
+  //         }
+  //
+  //         _.forEach(usageData, function (d) {
+  //           d.readableCount = numberWithCommas(d.doc_count);
+  //         });
+  //
+  //         applications = handleNullUsage(applications);
+  //         mapUsageDataToApplications(applications, usageData);
+  //   });
+  // };
 
-  // retreive a list of all apps
-  Applications.all().then(function(applications) {
-    $scope.app.applications = applications;
-    $scope.state = (applications.length) ? 'table' : 'blank';
-    getUsageData(applications);
-  });
+  // var handleNullUsage = function (applications) {
+  //   _.forEach(applications, function (application) {
+  //     application.usage = {
+  //       doc_count: 0,
+  //       readableCount: 0
+  //     };
+  //   });
+  //
+  //   return applications;
+  // };
+
+  // var mapUsageDataToApplications = function (applications, usageData) {
+  //   applications = _.map(applications, function (application) {
+  //     _.forEach(usageData, function (d) {
+  //       if (application.credentials.apiKey === d.key) {
+  //         application.usage = d;
+  //       }
+  //     });
+  //   });
+  // };
 
   // instantiate a new modal window
   $scope.createApplication = function() {
--- atlas-admin-frontend/app/presentation/applications/createModal/applicationCreateModal.ctrl.js
+++ atlas-admin-frontend/app/presentation/applications/createModal/applicationCreateModal.ctrl.js
@@ -1,87 +1,110 @@
 'use strict';
 
-angular.module('atlasAdmin.applications')
-.controller('CreateApplicationFormModalCtrl', ['$scope', '$q', '$sce', '$uibModalInstance', 'Applications', 'sourceRequests', 'SourceLicenses', '$location',
-    function($scope, $q, $sce, $uibModalInstance, Applications, SourceRequests, SourceLicenses, $location) {
-    $scope.app.showTerms = false;
-    $scope.app.acceptTerms = false;
-    $scope.app.title = '';
-    $scope.app.url = '';
-    $scope.app.description = '';
-    $scope.app.preset = null;
-    $scope.license = {};
-    $scope.license.show = false;
+angular
+  .module('atlasAdmin.applications')
+  .controller('CreateApplicationFormModalCtrl', CreateApplicationFormModalCtrl);
 
-    var getTerms = function(source) {
-        var defer = $q.defer();
-        var sourceId = null;
-        if (source === 'PA') {
-            sourceId = 'cpc';
-        }else if (source === 'BBC') {
-            sourceId = 'cpy';
+CreateApplicationFormModalCtrl.$inject = ['$scope', '$q', '$sce', '$uibModalInstance', 'Applications', 'sourceRequests', 'SourceLicenses', '$location'];
+
+function CreateApplicationFormModalCtrl($scope, $q, $sce, $uibModalInstance, Applications, SourceRequests, SourceLicenses, $location) {
+  $scope.app.showTerms = false;
+  $scope.app.acceptTerms = false;
+  $scope.app.title = '';
+  $scope.app.url = '';
+  $scope.app.description = '';
+  $scope.app.preset = null;
+  $scope.license = {};
+  $scope.license.show = false;
+
+  var getTerms = function(source) {
+    var defer = $q.defer();
+    var sourceId = null;
+
+    if (source === 'PA') {
+      sourceId = 'cpc';
+    } else if (source === 'BBC') {
+      sourceId = 'cpy';
+    }
+
+    SourceLicenses
+      .get(sourceId)
+      .then(function(data) {
+        if (!_.isObject(data)) {
+          return false
         }
-        SourceLicenses.get(sourceId).then(function(data) {
-            if (!_.isObject(data)) {return false}
-            var license = $sce.trustAsHtml(data.license);
-            defer.resolve(license);
-        })
-        return defer.promise;
-    }
 
-    // decide whether terms should be shown for this source set
-    $scope.termsToggle = function(preset) {
-        $scope.app.showTerms = ($scope.app.preset == 'uk')
-    }
+        var license = $sce.trustAsHtml(data.license);
+        defer.resolve(license);
+    });
 
-    // used to show the user terms for source
-    $scope.showTerms = function(source) {
-        getTerms(source).then(function(license) {
-            $scope.license.show = true;
-            $scope.license.html = license;
-        })
-    }
+    return defer.promise;
+  };
 
-    $scope.submit = function() {
-        var app_title       = $scope.app.title,
-            app_url         = $scope.app.url,
-            app_description = $scope.app.description,
-            app_preset      = $scope.app.preset,
-            app_terms       = $scope.app.acceptTerms;
+  // decide whether terms should be shown for this source set
+  $scope.termsToggle = function(preset) {
+    $scope.app.showTerms = ($scope.app.preset == 'uk')
+  };
 
-        // save the app data
-        if (!_.isEmpty(app_title) && !_.isEmpty(app_url) && _.isString(app_preset)) {
-            if (app_preset === 'uk' && !app_terms) return;
-            Applications.create(app_title, app_description, app_url).then(function(result) {
-                if (result.data.application.id) {
-                    var appId = result.data.application.id;
-                    // enable basic sources matching on simple account
-                    if (app_preset === 'uk') {
-                        var _item, sourceOrder = [], enableSources = [];
-                        for (var source in result.data.application.sources.reads) {
-                            _item = result.data.application.sources.reads[source];
-                            if (_item.title === 'BBC' || _item.title === 'PA') {
-                                enableSources.push(_item);
-                            }
-                            sourceOrder.push(_item.id);
-                        }
-                        // send source requests for default sources
-                        _(enableSources).forEach(function(src) {
-                            SourceRequests.send(src.id, appId, app_url, '', 'personal', true);
-                        })
-                        Applications.setPrecedence(appId, sourceOrder);
-                    }else{
-                        $location.path('/applications/'+appId);
-                    }
-                    // close modal and return data tot he $scope
-                    result.data.application.source = $scope.app.sources;
-                    $uibModalInstance.close(result.data.application);
-                }
+  // used to show the user terms for source
+  $scope.showTerms = function(source) {
+    getTerms(source)
+      .then(function(license) {
+        $scope.license.show = true;
+        $scope.license.html = license;
+    });
+  };
+
+  $scope.submit = function() {
+    var app_title = $scope.app.title;
+    var app_url = $scope.app.url;
+    var app_description = $scope.app.description;
+    var app_preset = $scope.app.preset;
+    var app_terms = $scope.app.acceptTerms;
+
+    // save the app data
+    if (!_.isEmpty(app_title) && !_.isEmpty(app_url) && _.isString(app_preset)) {
+      if (app_preset === 'uk' && !app_terms) {
+        return;
+      }
+
+      Applications.create(app_title, app_description, app_url).then(function(result) {
+        if (result.data.application.id) {
+          var appId = result.data.application.id;
+
+          // enable basic sources matching on simple account
+          if (app_preset === 'uk') {
+            var _item, sourceOrder = [], enableSources = [];
+
+            for (var source in result.data.application.sources.reads) {
+              _item = result.data.application.sources.reads[source];
+
+              if (_item.title === 'BBC' || _item.title === 'PA') {
+                enableSources.push(_item);
+              }
+
+              sourceOrder.push(_item.id);
+            }
+
+            // send source requests for default sources
+            _(enableSources).forEach(function(src) {
+              SourceRequests.send(src.id, appId, app_url, '', 'personal', true);
             });
-        }
-    };
 
-    // cancel and close modal
-    $scope.cancel = function() {
-        $uibModalInstance.dismiss('cancel');
-    };
-}]);
+            Applications.setPrecedence(appId, sourceOrder);
+          } else {
+            $location.path('/applications/'+appId);
+          }
+
+          // close modal and return data to the $scope
+          result.data.application.source = $scope.app.sources;
+          $uibModalInstance.close(result.data.application);
+        }
+      });
+    }
+  };
+
+  // cancel and close modal
+  $scope.cancel = function() {
+    $uibModalInstance.dismiss('cancel');
+  };
+}
