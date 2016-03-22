a101ebd1eea0fec753160bb19e5b0dde9789cc8b
Steve Rydz <steverydz@gmail.com>
2016-03-22 17:18:07 +0000

    Formatting

--- atlas-admin-frontend/app/presentation/wishlist/wishlistFeatures.ctrl.js
+++ atlas-admin-frontend/app/presentation/wishlist/wishlistFeatures.ctrl.js
@@ -1,70 +1,83 @@
 'use strict';
-var app = angular.module('atlasAdmin.wishlist');
 
-app.controller('CtrlWishlistFeatures', ['$scope', '$rootScope', '$routeParams', 'Wishes', '$q', '$uibModal',
-    function ($scope, $rootScope, $routeParams, Wishes, $q, $uibModal) {
-    var root = $rootScope;
-    $scope.features = {};
-    $scope.asked = {};
+var app = angular
+  .module('atlasAdmin.wishlist')
+  .controller('CtrlWishlistFeatures', CtrlWishlistFeatures);
 
-    root.$watch('wishlist', function(new_val, old_val) {
-        $scope.features = _.filter($rootScope.wishlist, function(n) {
-            return n.type === 'feature';
-        })
+CtrlWishlistFeatures.$inject = ['$scope', '$rootScope', '$routeParams', 'Wishes', '$q', '$uibModal'];
+
+function CtrlWishlistFeatures($scope, $rootScope, $routeParams, Wishes, $q, $uibModal) {
+  var root = $rootScope;
+
+  $scope.features = {};
+  $scope.asked = {};
+
+  root.$watch('wishlist', function(new_val, old_val) {
+    $scope.features = _.filter($rootScope.wishlist, function(n) {
+      return n.type === 'feature';
     });
-    root.$watch('wishes', function(new_val, old_val) {
-        $scope.asked = _.filter(root.wishes, function(n) {
-            return n.wish.type === 'feature';
-        })
-    });
+  });
 
-    $scope.user_has = function(item_id) {
-        var t = _.filter($scope.asked, {wish: { _id: item_id }});
-        return t.length > 0;
+  root.$watch('wishes', function(new_val, old_val) {
+    $scope.asked = _.filter(root.wishes, function(n) {
+      return n.wish.type === 'feature';
+    });
+  });
+
+  $scope.user_has = function(item_id) {
+    var t = _.filter($scope.asked, {wish: { _id: item_id }});
+    return t.length > 0;
+  }
+
+  $scope.make_wish = function(featureId, reason) {
+    var item = _.filter($scope.features, function(n) {
+      return n._id === featureId;
+    })[0];
+
+    if ('object' !== typeof item) {
+      throw new TypeError();
     }
 
-    $scope.make_wish = function(featureId, reason) {
-        var item = _.filter($scope.features, function(n) {
-            return n._id === featureId;
-        })[0];
-        if ('object' !== typeof item) throw new TypeError();
-        var postdata = {
-            wish: item,
-            reason: reason
-        }
-        Wishes.create(postdata).then(function(data) {
-            $scope.asked.push(data);
-        });
-    }
-
-    $scope.customFeatureWish = function() {
-        $scope.modal = {
-            title: 'Tell us about a feature'
-        }
-        var modalInstance = $uibModal.open({
-            templateUrl: 'presentation/wishlist/customFeatureRequestModal/customFeatureRequestModal.tpl.html',
-            controller: 'customFeatureRequestModal',
-            scope: $scope
-        });
+    var postdata = {
+      wish: item,
+      reason: reason
     };
-}]);
 
-app.directive('featureRow', ['$document', function($document) {
+    Wishes.create(postdata).then(function(data) {
+      $scope.asked.push(data);
+    });
+  };
+
+  $scope.customFeatureWish = function() {
+    $scope.modal = {
+      title: 'Tell us about a feature'
+    };
+
+    var modalInstance = $uibModal.open({
+      templateUrl: 'presentation/wishlist/customFeatureRequestModal/customFeatureRequestModal.tpl.html',
+      controller: 'customFeatureRequestModal',
+      scope: $scope
+    });
+  };
+}
+
+app
+  .directive('featureRow', ['$document', function($document) {
     var template =
-            '<td class="feature-item">'+
-                '<div class="feature-name panel-half"><h2>{{feature.title}}</h2></div>'+
-                '<div class="feature-options panel-half">'+
-                    '<span ng-if="!user_has(feature._id)" data-title="{{feature.title}}" input-morph="{{feature._id}}" class="button-to-input"></span>'+
-                    '<span ng-if="user_has(feature._id)" class="button medium stroke disabled">Requested</span>'+
-                '</div>'+
-                '<div class="feature-detail panel-full">'+
-                    '<div class="panel-half feature-description"><p>{{feature.feature.description}}</p></div>'+
-                    '<div class="panel-half"></div>'+
-                '</div>'+
-            '</td>';
+      '<td class="feature-item">'+
+        '<div class="feature-name panel-half"><h2>{{feature.title}}</h2></div>'+
+        '<div class="feature-options panel-half">'+
+          '<span ng-if="!user_has(feature._id)" data-title="{{feature.title}}" input-morph="{{feature._id}}" class="button-to-input"></span>'+
+          '<span ng-if="user_has(feature._id)" class="button medium stroke disabled">Requested</span>'+
+        '</div>'+
+        '<div class="feature-detail panel-full">'+
+          '<div class="panel-half feature-description"><p>{{feature.feature.description}}</p></div>'+
+          '<div class="panel-half"></div>'+
+        '</div>'+
+      '</td>';
 
     return {
-        scope: false,
-        template: template
-    }
+      scope: false,
+      template: template
+    };
 }]);
