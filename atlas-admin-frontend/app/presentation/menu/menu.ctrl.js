6748d1f0091501a72a985bee9b76c9fd0a871cd4
Steve Rydz <steverydz@gmail.com>
2016-03-23 10:32:48 +0000

    Formatting

--- atlas-admin-frontend/app/presentation/menu/menu.ctrl.js
+++ atlas-admin-frontend/app/presentation/menu/menu.ctrl.js
@@ -1,106 +1,135 @@
 'use strict';
 
-angular.module('atlasAdmin.menu')
-  .controller('UserMenuController', ['$scope', 'Users', '$rootScope', 'Authentication', '$location', 'GroupsService', '$q',
-    function($scope, Users, $rootScope, Authentication, $location, Groups, $q) {
-    var privateItems;
-    $scope.app = {};
+angular
+  .module('atlasAdmin.menu')
+  .controller('UserMenuController', UserMenuController);
+
+UserMenuController.$inject = ['$scope', 'Users', '$rootScope', 'Authentication', '$location', 'GroupsService', '$q'];
+
+function UserMenuController($scope, Users, $rootScope, Authentication, $location, Groups, $q) {
+  var privateItems;
+
+  $scope.app = {};
+  $scope.app.dropdown = true;
+  $scope.app.adminMenu = true;
+  $scope.app.contentMenu = true;
+  $scope.app.appsMenu = true;
+
+  $scope.app.showDropdown = function() {
     $scope.app.dropdown = true;
+  };
+
+  $scope.app.showAdminMenu = function() {
     $scope.app.adminMenu = true;
+  };
+
+  $scope.app.showContentMenu = function() {
     $scope.app.contentMenu = true;
+  };
+
+  $scope.app.showAppsMenu = function() {
     $scope.app.appsMenu = true;
+  };
 
-    $scope.app.showDropdown = function () {
-        $scope.app.dropdown = true;
-    };
+  $scope.app.hideDropdown = function() {
+    $scope.app.dropdown = false;
+  };
 
-    $scope.app.showAdminMenu = function () {
-        $scope.app.adminMenu = true;
-    };
+  $scope.app.hideAdminMenu = function() {
+    $scope.app.adminMenu = false;
+  };
 
-    $scope.app.showContentMenu = function () {
-        $scope.app.contentMenu = true;
-    };
+  $scope.app.hideContentMenu = function() {
+    $scope.app.contentMenu = false;
+  };
 
-    $scope.app.showAppsMenu = function () {
-        $scope.app.appsMenu = true;
-    };
+  $scope.app.hideAppsMenu = function() {
+    $scope.app.appsMenu = false;
+  };
 
-    $scope.app.hideDropdown = function () {
-        $scope.app.dropdown = false;
-    };
+  var getPrivateMenuItems = function() {
+    var defer = $q.defer();
 
-    $scope.app.hideAdminMenu = function () {
-        $scope.app.adminMenu = false;
-    };
-
-    $scope.app.hideContentMenu = function () {
-        $scope.app.contentMenu = false;
-    };
-
-    $scope.app.hideAppsMenu = function () {
-        $scope.app.appsMenu = false;
-    };
-
-    var getPrivateMenuItems = function() {
-        var defer = $q.defer();
-        if (privateItems) {
-            defer.resolve(privateItems)
-            return defer.promise;
-        }
-        Groups.get().then(function(result) {
-             privateItems = result;
-             defer.resolve(privateItems);
-        })
-        return defer.promise;
+    if (privateItems) {
+      defer.resolve(privateItems)
+      return defer.promise;
     }
 
-    var buildMenu = function(user, groups) {
-        // if profile not complete the do not show menu
-        var allMenu = [
-            // admin only
-            {path:'/manage/sources', label:'Sources', role:'admin'},
-            {path:'/manage/requests', label:'Requests', role:'admin'},
-            {path:'/manage/users', label:'Users', role:'admin'},
-            {path:'/manage/usage', label:'API Usage', role:'admin'},
-            {path:'/manage/wishlist', label:'Wishlist', role:'admin'}];
+    Groups
+      .get()
+      .then(function(result) {
+        privateItems = result;
+        defer.resolve(privateItems);
+    });
 
-        if (_.isArray(groups)) {
-            groups.forEach(function(item) {
-                if (item.name === 'BTBlackout') { allMenu.push({path: '/epg/bt-tv', label: 'EPG'}); }
-                if (item.name === 'BBC-YV-Feed') { allMenu.push({path: '/feeds', label: 'Feeds'}); }
-                if (item.name === 'BBC-Scrubbables') { allMenu.push({path: '/scrubbables', label: 'Scrubbables'}); }
-            })
+    return defer.promise;
+  };
+
+  var buildMenu = function(user, groups) {
+    // if profile not complete the do not show menu
+    var allMenu = [
+      // admin only
+      { path:'/manage/sources', label:'Sources', role:'admin' },
+      { path:'/manage/requests', label:'Requests', role:'admin' },
+      { path:'/manage/users', label:'Users', role:'admin' },
+      { path:'/manage/usage', label:'API Usage', role:'admin' },
+      { path:'/manage/wishlist', label:'Wishlist', role:'admin' }
+    ];
+
+    if (_.isArray(groups)) {
+      groups.forEach(function(item) {
+        if (item.name === 'BTBlackout') {
+          allMenu.push({
+            path: '/epg/bt-tv',
+            label: 'EPG'
+          });
         }
 
-        // build the menu
-        var menu = [];
-        var admin_menu = [];
-        for (var i = 0; i < allMenu.length; i++) {
-            var item = allMenu[i];
-            if (!item.hasOwnProperty('role') || item.role === 'regular') {
-                menu.push(item);
-            }else if (user.role === 'admin') {
-                admin_menu.push(item);
-            }
+        if (item.name === 'BBC-YV-Feed') {
+          allMenu.push({
+            path: '/feeds',
+            label: 'Feeds'
+          });
         }
-        return {
-            users: menu,
-            admins: admin_menu
+
+        if (item.name === 'BBC-Scrubbables') {
+          allMenu.push({
+            path: '/scrubbables',
+            label: 'Scrubbables'
+          });
         }
+      });
+    }
+
+    // build the menu
+    var menu = [];
+    var admin_menu = [];
+
+    allMenu.forEach(function(item) {
+      if (!item.hasOwnProperty('role') || item.role === 'regular') {
+        menu.push(item);
+      } else if (uesr.role === 'admin') {
+        admin_menu.push(item);
+      }
+    });
+
+    return {
+      users: menu,
+      admins: admin_menu
     };
+  };
 
-    if (Authentication.getToken()) {
-        Users.currentUser(function(user) {
-            $scope.app.user = user;
-            // find any custom menu items for this user
-            getPrivateMenuItems().then(function(groups) {
-                $scope.app.userGroups = groups;
-                $scope.app.menu = buildMenu(user, groups);
-            },
-            function() {
-                $scope.app.menu = buildMenu(user);
-            });
+  if (Authentication.getToken()) {
+    Users.currentUser(function(user) {
+      $scope.app.user = user;
+      // find any custom menu items for this user
+      getPrivateMenuItems()
+        .then(function(groups) {
+          $scope.app.userGroups = groups;
+          $scope.app.menu = buildMenu(user, groups);
+        }, function() {
+          $scope.app.menu = buildMenu(user);
         });
-    }
-}]);
+    });
+  }
+}
