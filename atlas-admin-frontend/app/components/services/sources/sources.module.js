ed49a5b92aabc96eca30214a16e8b6995c180d2d
Steve Rydz <steverydz@gmail.com>
2016-03-31 16:25:37 +0100

    Trying to make sources work

--- atlas-admin-frontend/app/components/services/sources/sources.module.js
+++ atlas-admin-frontend/app/components/services/sources/sources.module.js
@@ -1,3 +1,4 @@
 'use strict';
 
-angular.module('atlasAdmin.services.sources', []);
+angular
+  .module('atlasAdmin.services.sources', []);
--- atlas-admin-frontend/app/components/services/sources/sources.service.js
+++ atlas-admin-frontend/app/components/services/sources/sources.service.js
@@ -1,22 +1,45 @@
 'use strict';
 
-angular.module('atlasAdmin.services.sources')
-  .factory('Sources', function (Atlas, Applications, $log) {
-    return {
-        all: function () {
-             return Atlas.getRequest('/sources.json').then(function(result) {return result.data.sources;});
-        },
-        get: function (sourceId) {
-             return Atlas.getRequest('/sources/' + sourceId + '.json').then(function(result) {return result.data.source;});
-        },
-        changeAppState: function(sourceId, appId, state, callback) {
-            var data = {};
-            var url = "/sources/" + sourceId + "/applications/readers/" + appId + "/state?state="+ state;
-            Atlas.postRequest(url, data, {withCredentials: false}).success(callback).error(function(error) {$log.error(error)});
-        },
-        addWriter: function(sourceId, appId, callback) {
-            var url = "/sources/" + sourceId + "/applications?id=" + appId + "&permission=write";
-            Atlas.postRequest(url, {}, {withCredentials: false}).success(callback);
-        }
+angular
+  .module('atlasAdmin.services.sources')
+  .factory('Sources', Sources);
+
+function Sources(Atlas, Applications, $log) {
+  return {
+    all: function () {
+      return Atlas
+        .getRequest('http://admin-backend.metabroadcast.com/1/applications//sources')
+        .then(function(result) {
+          return result.data.sources;
+      });
+    },
+
+    get: function (sourceId) {
+      return Atlas
+        .getRequest('http://admin-backend.metabroadcast.com/1/sources/' + sourceId)
+        .then(function(result) {
+          return result.data.source;
+      });
+    },
+
+    changeAppState: function(sourceId, appId, state, callback) {
+      var data = {};
+      var url = "/sources/" + sourceId + "/applications/readers/" + appId + "/state?state="+ state;
+
+      Atlas
+        .postRequest(url, data, { withCredentials: false })
+        .success(callback)
+        .error(function(error) {
+          $log.error(error)
+      });
+    },
+
+    addWriter: function(sourceId, appId, callback) {
+      var url = "/sources/" + sourceId + "/applications?id=" + appId + "&permission=write";
+
+      Atlas
+        .postRequest(url, {}, { withCredentials: false })
+        .success(callback);
     }
-  });
+  }
+}
