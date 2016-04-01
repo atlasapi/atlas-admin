318f83e452baf4a71b947ebd89beef651dd7020d
Steve Rydz <steverydz@gmail.com>
2016-04-01 10:59:39 +0100

    Refactor auth service to only necessary components

--- atlas-admin-frontend/app/components/interceptors/profileComplete/profileComplete.interceptor.js
+++ atlas-admin-frontend/app/components/interceptors/profileComplete/profileComplete.interceptor.js
@@ -9,34 +9,34 @@ ProfileCompleteInterceptor.$inject = ['ProfileStatus', '$location', '$q', '$root
 function ProfileCompleteInterceptor(ProfileStatus, $location, $q, $rootScope, Auth) {
   return {
     request: function(config) {
-      var _url = config.url;
-      var _provider = Auth.getProvider() || null;
-      var _token = Auth.getToken() || null;
-
-      // some paths are just for use by the application; we don't want
-      // those to be included in redirects etc
-      var allowedRoute = function () {
-        return (_url.indexOf('/auth') === -1 &&
-          _url.indexOf('/logout') === -1 &&
-          _url.indexOf('/login') === -1 &&
-          _url.indexOf('/profile') === -1);
-      }
-
-      if (_provider && _token) {
-        if (!ProfileStatus.isProfileComplete() && allowedRoute()) {
-          $location.path('/profile');
-        }
-
-        if (ProfileStatus.getLicenseAccepted() === false && allowedRoute()) {
-          $location.path('/terms');
-        }
-      }
-
-      return config || $q.reject(config);
+    //   var _url = config.url;
+    //   var _provider = Auth.getProvider() || null;
+    //   var _token = Auth.getToken() || null;
+    //
+    //   // some paths are just for use by the application; we don't want
+    //   // those to be included in redirects etc
+    //   var allowedRoute = function () {
+    //     return (_url.indexOf('/auth') === -1 &&
+    //       _url.indexOf('/logout') === -1 &&
+    //       _url.indexOf('/login') === -1 &&
+    //       _url.indexOf('/profile') === -1);
+    //   }
+    //
+    //   if (_provider && _token) {
+    //     if (!ProfileStatus.isProfileComplete() && allowedRoute()) {
+    //       $location.path('/profile');
+    //     }
+    //
+    //     if (ProfileStatus.getLicenseAccepted() === false && allowedRoute()) {
+    //       $location.path('/terms');
+    //     }
+    //   }
+    //
+    //   return config || $q.reject(config);
     },
-
+    //
     response: function(response) {
-      return response || $q.reject(response);
+    //   return response || $q.reject(response);
     }
   }
 }
--- atlas-admin-frontend/app/components/services/auth/auth.service.js
+++ atlas-admin-frontend/app/components/services/auth/auth.service.js
@@ -1,50 +1,26 @@
 'use strict';
 
 angular
-  .module('atlasAdmin.services.auth')
+  .module('atlasAdmin.services.auth', ['ngCookies'])
   .factory('Authentication', Authentication);
 
-Authentication.$inject = ['$rootScope', 'ProfileStatus'];
+Authentication.$inject = ['$rootScope', '$cookies'];
 
-function Authentication($rootScope, ProfileStatus) {
+function Authentication($rootScope, $cookies) {
+  var authCookieName = 'iPlanetDirectoryPro';
+
   if (!$rootScope.status) {
     $rootScope.status = {};
   }
 
   return {
-    getProvider: function () {
-      return localStorage.getItem('auth.provider');
-    },
-
-    setProvider: function (provider) {
-      localStorage.setItem('auth.provider', provider);
-    },
-
     getToken: function () {
-      return localStorage.getItem('auth.token');
-    },
-
-    setToken: function (token) {
-      localStorage.setItem('auth.token', token);
+      return $cookies.get(authCookieName);
     },
 
     reset: function () {
-      localStorage.removeItem('auth.provider');
-      localStorage.removeItem('auth.token');
-      localStorage.removeItem('profile.complete');
-      localStorage.removeItem('license.accepted');
+      $cookies.remove(authCookieName);
       $rootScope.status.loggedIn = false;
-    },
-
-    appendTokenToUrl: function (url) {
-      var provider = localStorage.getItem('auth.provider');
-      var token = localStorage.getItem('auth.token');
-      var oauthParams = 'oauth_provider=' + provider + '&oauth_token=' + token;
-      if (!token) {
-        return url;
-      }
-      $rootScope.status.loggedIn = true;
-      return (url.indexOf('?') === -1) ? url + '?' + oauthParams : url + '&' + oauthParams;
     }
   };
 }
