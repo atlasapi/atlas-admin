617b0233b11603f966ddd8ad594905e0189a5084
Steve Rydz <steverydz@gmail.com>
2016-04-01 10:55:15 +0100

    Formatting

--- atlas-admin-frontend/app/components/services/auth/auth.module.js
+++ atlas-admin-frontend/app/components/services/auth/auth.module.js
@@ -1,3 +1,4 @@
 'use strict';
 
-angular.module('atlasAdmin.services.auth', ['atlasAdmin.services.profileStatus']);
+angular
+  .module('atlasAdmin.services.auth', ['atlasAdmin.services.profileStatus']);
--- atlas-admin-frontend/app/components/services/profileStatus/profileStatus.module.js
+++ atlas-admin-frontend/app/components/services/profileStatus/profileStatus.module.js
@@ -1,3 +1,4 @@
 'use strict';
 
-angular.module('atlasAdmin.services.profileStatus', []);
+angular
+  .module('atlasAdmin.services.profileStatus', []);
